3af62be77bf65760488732c4898198929d0d5c09
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2024-03-06 09:14:50 +0100

    Update about text

--- src/cli/cliopts.rs
+++ src/cli/cliopts.rs
@@ -5,7 +5,7 @@ use std::path::PathBuf;
 #[clap(name = "otoroshictl")]
 #[clap(author = "Mathieu ANCELIN (Cloud APIM)")]
 #[clap(version = "1.0.0")]
-#[clap(about = "Manage your otoroshi clusters with style. otoroshictl is a CLI that can interact with the admin api of an otoroshi cluster. \n\nYou can also use it to expose local process through the otoroshi remote tunnels feature and as an universal sidecar to create a service mesh based on otoroshi. otoroshictl also provide a nice integration with cloud-apim, the managed otoroshi instances provider. \n\notoroshictl is an open-source tool proudly provided by cloud-apim (https://www.cloud-apim.com). The sources of otoroshictl are available on github at https://github.com/cloud-apim/otoroshictl", long_about = None)]
+#[clap(about = "Manage your otoroshi clusters with style. otoroshictl is a CLI that can interact with the admin api of an otoroshi cluster. \n\nYou can also use it to expose local process through the otoroshi remote tunnels feature and as an universal sidecar to create a service mesh based on otoroshi. otoroshictl also provide a nice integration with cloud-apim, the managed otoroshi instances provider. \n\notoroshictl is an open-source tool proudly provided by Cloud APIM (https://www.cloud-apim.com). Cloud APIM is a company that provides managed Otoroshi clusters and Wasmo instances perfectly configured and optimized, ready in seconds. The sources of otoroshictl are available on github at https://github.com/cloud-apim/otoroshictl", long_about = None)]
 #[clap(arg_required_else_help(true))]
 pub struct CliOpts {
 
