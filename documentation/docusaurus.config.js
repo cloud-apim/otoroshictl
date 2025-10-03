99c068bfb28fea881b4f473c09ae8bfc92b121f3
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2025-10-03 16:20:34 +0200

    build documentation website

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
         title: 'otoroshictl',
         logo: {
           alt: 'otoroshictl Logo',
-          src: 'img/otoroshictl-logo.png',
+          src: 'img/otoroshictl-logo-nav.png',
         },
         items: [
           {
